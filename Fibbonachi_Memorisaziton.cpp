9af606ea2a131f17cb85a8f29831c44ba9eb9f4b
Neeraj Choubisa <82640789+Kali-Decoder@users.noreply.github.com>
2021-09-19 01:58:30 +0530

    Add files via upload

---
+++ Fibbonachi_Memorisaziton.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+int F[5]={-1,-1,-1,-1,-1};
+int fib(int n)
+{   
+    if(n<=1)
+    {
+    F[n]=n;
+    return n;
+    }
+    else{
+        if(F[n-2]==-1)
+        {
+            F[n-2]=fib(n-2);
+        }
+        if(F[n-1]==-1)
+        {
+            F[n-1]=fib(n-1);
+        }
+        return F[n-2] + F[n-1];
+    }
+}
+int main()
+{   
+    cout<<fib(5);
+    return 0;
+}
---
+++ Taylor_Series.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+float e(int x, int n)
+{
+    static float p=1,f=1;
+    float r;
+
+    if(n==0)
+    return 1;
+    else{
+        r=e(x,n-1);
+        p=p*x;
+        f=f*n;
+        return r+ p/f;
+    }
+}
+int main()
+{   
+    cout<<e(1,3)<<endl;;
+    return 0;
+}
